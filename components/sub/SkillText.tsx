64ae6db25d320ef2a3b711b283dd415618a52ead
Erlendurx <erlanleon10@gmail.com>
2024-09-27 17:47:35 -0400

    c1

--- components/sub/SkillText.tsx
+++ components/sub/SkillText.tsx
@@ -1,5 +1,4 @@
 "use client"
-
 import React from 'react'
 import {motion} from 'framer-motion'
 import { slideInFromLeft, slideInFromRight, slideInFromTop } from '@/utils/motion'
@@ -8,26 +7,26 @@ import { SparklesIcon } from '@heroicons/react/24/solid'
 const SkillText = () => {
   return (
     <div className='w-full h-auto flex flex-col items-center justify-center'>
-        <motion.div
-        variants={slideInFromTop}
-        className='Welcome-box py-[8px] px-[8px] border border-[#7042f88b] opacity-[0.9]'
+<motion.div
+          variants={slideInFromTop}
+          className="Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]"
         >
-            <SparklesIcon className='text-[#b49bff] mr-[10px] h-5 w-5' />
-            <h1 className='Welcome-text text-[13px]'>
-                Hecho con Next js 13
-            </h1>
+          <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
+          <h1 className="Welcome-text text-[13px]">
+            Think better with Next js 13
+          </h1>
         </motion.div>
         <motion.div
         variants={slideInFromLeft(0.5)}
         className='text-[30px] text-white font-medium mt-[10px] text-center mb-[15px]'
-        > 
-            Realizando aplicaciones con tecnologia moderna
+        >
+            Making apps with modern technologies
         </motion.div>
         <motion.div
         variants={slideInFromRight(0.5)}
         className='cursive text-[20px] text-gray-200 mb-10 mt-[10px] text-center'
         >
-            Texto de prueba "Pensar en algo para suplantarlo aqui"
+            Never miss a task, deadline or idea
         </motion.div>
     </div>
   )
